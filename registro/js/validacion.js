aa7685702672a6c69e759bd28ffdcb0c4d32caa9
akihito <akihito2000h@gmail.com>
2025-08-14 14:45:41 -0300

    alerta-email

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function showAlertEmail() {
+    document.getElementById("alert-email").classList.add("show");
+}
+
+//validacion de email al hacer click en registrarme.
+document.getElementById("regBtn").addEventListener("click", function(e){
+    e.preventDefault();
+const emailInput = document.getElementById("email");
+const email = emailInput.value.trim();
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if (!emailPattern.test(email)){
+    showAlertEmail();
+}
+})

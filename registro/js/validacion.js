4d0c53e4fbe3f5a8d6cb0ba1a34a36a87520bb40
Beluway <morenosuarezmariabelen@gmail.com>
2025-08-14 14:14:25 -0300

    belen

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,23 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    const boton= document.querySelector("#regBtn");
+boton.addEventListener("click",function(e){
+    let pass1 = document.querySelector("#password1").value;
+    let pass2 = document.querySelector("#password2").value;
+
+    let alert = document.getElementById("alert");
+    alert.innerHTML="";
+
+    if(pass1!==pass2){
+        alert.innerHTML="Las contraseñas no coinciden";
+        e.preventDefault();
+        showAlertError();
+    }
+})});
+
+    
